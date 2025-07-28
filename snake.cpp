e32333442c289f36e19ded767c9d4882cb623e78
unknown <as6323@ic.ac.uk>
2025-07-28 19:58:58 +0200

    Created first files - added snake constructor and few methods

---
+++ snake.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+#include "snake.h"
+
+using namespace std;
+
+// Define an alias for the data type
+typedef vector<pair<int,int>> vec_pair; 
+
+// Constructor - Create snake with length 2 at (0,0)
+snake::snake() : length(2), pos_x(0), pos_y(0){
+    // Add the current position of snake to the variable "body"
+    snake::addPosToBody(pos_x, pos_y);
+    cout << "Snek created!" << "\n";
+}
+
+// Getter method to retrieve current snake positions 
+vec_pair snake::getBodyPos(){
+    return snake::body;
+}
+
+// Setter method to append added body position to the variable "body"
+void snake::addPosToBody(int x, int y){
+    snake::body.push_back(make_pair(x,y));
+}
+
+// Getter method to retrieve current length of snake
+int snake::getLength(){
+    return snake::length;
+}
+
+int main(){
+    snake mySnek;
+    cout << "Length of snek is: " << mySnek.getLength() << "\n"; 
+
+    return 0;
+}
---
+++ snake.h
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+typedef vector<pair<int,int>> vec_pair; 
+
+class snake{
+    private:
+        int length;
+        int pos_x; 
+        int pos_y;
+        vec_pair body;
+        
+    public:
+        snake();
+        int getLength();
+        vec_pair getBodyPos();
+        void addPosToBody(int x, int y);
+};
